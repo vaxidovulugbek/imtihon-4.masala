500644ce986fe18f67210b8d70014b1ce1c362c2
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-04-06 10:36:09 +0500

    commit

---
+++ main.js
@@ -0,0 +1,198 @@
+// massivni ohirgi elementini topish
+// let a = [2,3,88,32,9]
+// console.log(a[a.length-1]);
+
+// arraydagi eng katta sonni topish
+// var array = [3 , 6, 2, 56,99, 32, 5, 89, 32];
+// var largest= 0;
+// for (i=0; i<=largest;i++){
+//     if (array[i]>largest) {
+//         largest=array[i];
+//     }
+// }
+// console.log(largest);
+
+
+// son honalarga bolib chiqish
+// let a = 123456;
+// let elCount = 0;
+// for(; a > 0;){
+//   let c = a % 10;
+//   a = Math.floor(a / 10);
+//   console.log(c);
+//   elCount++;
+// }
+// console.log(`${elCount} xonali son`);
+
+
+// son hanalari orasida eng katta sonit topish
+// let a = 1238456;
+// let elCount = 0;
+// let k = a % 10;
+// for(; a > 0;){
+//   let c = a % 10;
+//   a = Math.floor(a / 10);
+//   console.log(c);
+//   elCount++;
+//   if(k < c){
+//     k = c;
+//   }
+// }
+// console.log(`${elCount} xonali son`);
+// console.log(`Eng katta son ${k}`);
+
+// fibonacci shekilli
+// let a = [2,4,6,8]
+// let c
+// for (let i = 0; i < a.length; i++) {
+//    c = (a[i] + a[i - 1]);  
+// }
+
+
+// let input = document.querySelector("#input")
+// input.addEventListener("keyup",() => {
+//   console.log(input.value);
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
