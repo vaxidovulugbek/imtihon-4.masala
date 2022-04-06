5b6be36a990696201a9c1236990130780ef1163d
vakhidovulugbek03 <vulugbekk@gmail.com>
2022-04-06 14:02:03 +0500

    commit

--- main.js
+++ main.js
@@ -1,106 +1,194 @@
-// massivni ohirgi elementini topish
-// let a = [2,3,88,32,9]
-// console.log(a[a.length-1]);
 
-// arraydagi eng katta sonni topish
-// var array = [3 , 6, 2, 56,99, 32, 5, 89, 32];
-// var largest= 0;
-// for (i=0; i<=largest;i++){
-//     if (array[i]>largest) {
-//         largest=array[i];
-//     }
-// }
-// console.log(largest);
-
-
-// son honalarga bolib chiqish
-// let a = 123456;
-// let elCount = 0;
-// for(; a > 0;){
-//   let c = a % 10;
-//   a = Math.floor(a / 10);
-//   console.log(c);
-//   elCount++;
-// }
-// console.log(`${elCount} xonali son`);
-
-
-// son hanalari orasida eng katta sonit topish
-// let a = 1238456;
-// let elCount = 0;
-// let k = a % 10;
-// for(; a > 0;){
-//   let c = a % 10;
-//   a = Math.floor(a / 10);
-//   console.log(c);
-//   elCount++;
-//   if(k < c){
-//     k = c;
+// 1 masala togri
+// let arr = [10,1,3,5,8,9]
+// for (let i = 0; i < arr.length; i++) {
+// for (let j = 0; j < arr.length; j++) {
+//   if (arr[j] > arr[j + 1]) {
+//     let a = arr[j + 1]
+//     arr[j + 1] = arr[j] 
+//     arr[j]= a
 //   }
 // }
-// console.log(`${elCount} xonali son`);
-// console.log(`Eng katta son ${k}`);
-
-// fibonacci shekilli
-// let a = [2,4,6,8]
-// let c
-// for (let i = 0; i < a.length; i++) {
-//    c = (a[i] + a[i - 1]);  
 // }
+// console.log(arr)
 
 
-// let input = document.querySelector("#input")
-// input.addEventListener("keyup",() => {
-//   console.log(input.value);
+
+// 2 masala
+// let elArr = [
+//   {
+//     login : " forever    ",
+//     pass : "forever123"
+//   }
+// ];
+// elArr.forEach((item) => {
+//   if(item.login.trim().length < 3 && item.pass.trim().length < 8){
+//     console.log("login 3ta belgidam iborat bolish kerak kamida");
+//     console.log("parol 8ta belgidan iborat bolish kere");
+//   }
+//   else if(item.login.trim().length < 3){
+//     console.log("login kamida 3ta belgidan iborat bolish kere");
+//   } else if(item.pass.trim().length < 8){
+//     console.log("parol kamida 8ta belgidan iborat bolish kerak");
+//   } else{
+//     console.log("Siz muvaffaqqiyatli ro'yxatdan o'tdingiz");
+//   }
 // })
 
 
 
+// 3 masal problem 4 
+// let n = 10
+// let k = 2
+// let Sum = 0
+
+// for(let i = 1; i <= n; i++){
+//   if(i > 1){
+//     let elsumma = 1;
+//     for(let j = 0; j < k; j++){
+//       elsumma *= i;
+//     }
+//     Sum += elsumma;
+//   } else if(i == 1){
+//     Sum += i;
+//   }
+// }
+// console.log(Sum);
+
+
+// 4 masala problem 6
+// let newarr = [7,5,3,2];
+// let b = Math.floor(newarr.length / 2);
+// let k = b;
+// for(let i = 0; i < b; i++){
+//   let c = newarr[i];
+//   newarr[i] = newarr[k];
+//   newarr[k] = c;
+//   k++;
+// }
+// console.log(newarr);
+
+
+// 5 masala problem5
+// let arr = [1,[2,3],undefined,{x: 1}];
+// let arr2 = [];
+// for(let i = 0; i < arr.length; i++){
+//   if(Number.isInteger(arr[i])){
+//     arr2.push(arr[i]);
+//   } else if (Array.isArray(arr[i])){
+//     arr[i].forEach((item) => {
+//       arr2.push(item);
+//     })
+//   } else if(typeof arr[i] === "object"){
+//     arr2.push(Object.keys(arr[i])[0]);
+//     arr2.push(Object.values(arr[i])[0]);
+//   } else{
+//     arr2.push(arr[i]);
+//   }
+// }
+// console.log(arr2);
 
 
 
 
 
+// // DOM
+let input = document.querySelector("#input")
+let spetial = document.querySelector("#Spetial")
+let btnxspetial = document.querySelector("#btnxspetial")
+let eye = document.querySelector("#eye")
+let list = document.querySelector("#list")
+let elNumber = document.querySelector("#Number")
+let btnnum = document.querySelector("#btnnum")
 
 
+input.addEventListener("keyup" ,(e) => {
+  e.preventDefault()
+  list.classList.add("db")
 
+  let a = input.value.includes("@")
+  let elq = input.value.includes(1)
+  let elw = input.value.includes(2)
+  let ele = input.value.includes(3)
+  let elr = input.value.includes(4)
+  let elt = input.value.includes(5)
+  let ely = input.value.includes(6)
+  let elu = input.value.includes(7)
+  let eli = input.value.includes(8)
+  let elo = input.value.includes(9)
+  let elp = input.value.includes(0)
 
+  // console.log(elq);
 
+  if (a == true) {
+    spetial.classList.add("ff")
+    btnxspetial.classList.add("ff")
+  }
+  else if (a != true) {
+    spetial.classList.remove("ff")
+    btnxspetial.classList.remove("ff")
+  }
+// 111111111111111
+  if (elq == true) {
+    console.log("bir");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
+  else if (elq != true) {
+    console.log("bir");
+  }
+// llllllllllll
+  if (elw == true) {
+    console.log("ikki");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
+// pppppppppppppp
 
+  if (ele == true) {
+    console.log("uch");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
+  if (elr == true) {
+    console.log("tort");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
+  if (elt == true) {
+    console.log("besh");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
+  if (ely == true) {
+    console.log("olti");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
+  if (elu == true) {
+    console.log("yetti");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
+  if (eli == true) {
+    console.log("sakkiz");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
+  if (elo == true) {
+    console.log("toqqiz");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
+  if (elp == true) {
+    console.log("nol");
+    elNumber.classList.add("ff")
+    btnnum.classList.add("ff")
+  }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // console.log(spetial.textContent);
+    // console.log(input.value.lenght);
+})
 
 
 
